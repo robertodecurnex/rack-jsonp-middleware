08dc384f342fa6f8c3e19be93ca9f78ab25d5f45
robertodecurnex <nex.development@gmail.com>
2010-12-28 16:58:32 -0300

    Adding specs, Adding Bundler, Fixing some rack specification errors

--- lib/rack/jsonp.rb
+++ lib/rack/jsonp.rb
@@ -1,30 +1,33 @@
 module Rack
+
   class JSONP
-    include Rack::Utils
 
     def initialize(app)
       @app = app
     end
 
-
     def call(env)
       request = Rack::Request.new(env)
-      requesting_jsonp = Pathname(request.env['REQUEST_PATH']).extname =~ /^\.jsonp$/i
+      requesting_jsonp = Pathname(request.env['PATH_INFO']).extname =~ /^\.jsonp$/i
       callback = request.params['callback']
 
       return [400,{},[]] if requesting_jsonp && !callback
 
-      env['REQUEST_URI'].sub!(/\.jsonp/i, '.json') if requesting_jsonp
+      env['PATH_INFO'].sub!(/\.jsonp/i, '.json') if requesting_jsonp
       
-      status, headers, response = @app.call(env)
+      status, headers, body = @app.call(env)
 
       if requesting_jsonp
-        response.body = "#{callback}(#{response.body});"
-        headers['Content-Length'] = response.body.length.to_s
+        json = ""
+        body.each { |s| json << s }
+        body = ["#{callback}(#{json});"]
+        headers['Content-Length'] = body[0].length.to_s
         headers['Content-Type'] = 'application/javascript'
       end
 
-      [status, headers, response]
+      [status, headers, body]
     end
+
   end
+
 end
